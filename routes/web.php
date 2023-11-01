dcf249ae2435b4f6e0f3ebb98d8fd88d3d21293b
nanohana55 <nanaha.0505@gmail.com>
2023-11-01 17:44:53 +0900

    ブログ投稿作成画面の作成

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('/test', function () {
 });
 
 Route::get('/',[PostController::class,'index']);
-
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}',[PostController::class ,'show']);
